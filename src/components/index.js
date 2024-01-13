877afb865c5252b15245e54c1e395d6603f63175
Lalit <shankhwarlalit0@gmail.com>
2024-01-13 20:25:10 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Container from './container/Container';
+import Logo from './Logo';
+import LogoutBtn from './Header/LogoutBtn';
+import Button from './Button';
+import Input from './Input';
+import Select from './Select';
+import PostCard from './PostCard';
+import Login from './Login';
+import Signup from './Signup';
+import PostForm from './PostForm/PostForm'
+import AuthLayout from './AuthLayout';
+import RTE from './RTE';
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    PostCard,
+    Login,
+    Signup,
+    PostForm,
+    AuthLayout,
+    RTE
+};
